699fef47f84c54eef037eea5fb726ca6a3b4000d
Adrian Aguilar <Tygor23@gmail.com>
2021-06-02 12:30:47 -0700

    setting up files and linking

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Employee = require("./lib/Employee")
+const Manager = require("./lib/Manager")
+const Engineer = require("./lib/Engineer")
+const Intern = require("./lib/Intern")
